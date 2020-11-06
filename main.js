3cf280f8329b2f05390f6e3906143dd92cb0cc6d
renzosantamaria <renzo.santamaria@outlook.com>
2020-11-06 17:40:52 +0100

    kommentarer som små ändring av knapparnas class

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-
+//FUNKTION SOM DÖLJER ALLA SECTIONS GENOM ATT LÄGGA TILL "HIDDEN" TILL ALLA SEKTIONER
 function showSection() {
     let link = document.querySelectorAll("section")
     for (let i = 0; i < link.length; i++) {
         link[i].classList.add("hidden")
     }
-    
 }
     
+    
 
-let btn = document.querySelectorAll("button")
+let btn = document.querySelectorAll(".btn-wrapper button")//TAR KNAPPARNA HOME, SEARCH OCH INFO
 let currentClass;
 for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener("click", function(){
-        currentClass = "." + btn[i].innerHTML.toLowerCase()
-        console.log(currentClass);
-        
-        showSection()
-        document.querySelector(currentClass).classList.remove("hidden")
+    btn[i].addEventListener("click", function(){ // GER KNAPPARNA EN EVENT LISTENER
+        currentClass = "." + btn[i].innerHTML.toLowerCase()//HÄMTAR INNERTEXT OCH OMVANDLAR DET TILL LOWER CASE SAMT SPARAR DET I "CURRENTCLASS" VARIABELN
+    
+        showSection() // KALLAR PÅ FUNKTIONEN SOM LÄGGER TILL HIDDEN PÅ ALLA SEKTIONS
+        document.querySelector(currentClass).classList.remove("hidden")//TAR BORT HIDDEN FRÅN JUST DEN SEKTION MAN VÄLJER GENOM ATT KLICKA PÅ KNAPPEN
     })
 }
